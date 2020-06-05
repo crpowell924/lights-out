26da0de7fab2fc2f95edf0e14ec71d75d38965aa
Carlee Powell <crpowell@live.unc.edu>
2020-06-04 20:53:17 -0400

    Implemented whole game ha should have committed more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+import Game from './game'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="has-background-dark">
+      <Header/>
+      <div className="container">
+        <Game/>
+        <Footer/>
+      </div>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <section className="hero has-background-link">
+      <div className="hero-body has-text-centered">
+        <div className="container">
+          <h1 className="title is-1"><span className="has-text-light">LIGHTS</span>OUT</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function Footer() {
+  return(
+    <footer className="footer has-background-dark has-text-centered has-text-light">
+      <section className="container">
+        Remake of Lights Out by <a href="http://www.carleepowell.com/">Carlee Powell</a>, as requested by Viget.
+      </section>
+    </footer>
   );
 }
 
--- src/game.js
+++ src/game.js
@@ -1,24 +1,136 @@
 import React, {Component} from 'react';
 
+const easyWin = 
+[0,0,0,0,0,
+0,0,0,0,0,
+0,0,0,0,0,
+0,0,0,0,1,
+0,0,0,1,1];
 
-function Square(props) {
-  return(
-    <button className="square"
-    onClick={props.onClick}/>
-  );
+class Game extends Component {
+    // contains winning state, reset capability, tracks moves
+    constructor(props) {
+        super(props);
+        this.state = {
+            tiles: generateInitStates(),
+            didWinGame: false,
+            moves: 0
+        }
+    }
+    resetTiles() {
+        this.setState({
+            tiles : generateInitStates(),
+            didWinGame : false,
+            moves: 0
+        })
+    }
+    handleTileClick(ind) {
+        if(this.state.didWinGame === true){ return; }
+
+        var tiles = this.state.tiles.slice();
+        tiles = toggleTiles(tiles, ind);
+        this.setState({
+            tiles: tiles,
+            moves: this.state.moves + 1
+        });
+
+        if (didWinGame(tiles)===true) {
+            this.setState({didWinGame: true});
+            console.log("you won")
+            return;
+        }
+    }
+    render() {
+        var statusText;
+        var buttonText;
+        if (this.state.didWinGame === true) {
+            // TODO: maybe stop onClick from working?
+            statusText = "You won! Want to play again?";
+            buttonText = "Play Again";
+        } else {
+            statusText = "Turn off all the lights to win!";
+            buttonText = "Restart"
+        }
+        return (
+            <section className="has-text-centered section">
+                <div className="subtitle has-text-light">{statusText}</div>
+                <button className="button has-text-light has-background-link" 
+                    onClick={() => this.resetTiles()}>{buttonText}
+                </button>
+                <Board tiles={this.state.tiles} onClick={(ind) => this.handleTileClick(ind)}/>
+                <div className="subtitle has-text-light">Moves: {this.state.moves}</div>
+            </section>
+        )
+    }
 }
 
 class Board extends Component {
-  renderSquare(i) {
+    renderTile(ind) {
+        return(
+            <Tile 
+            key={ind}
+            value={this.props.tiles[ind]}
+            onClick={() => this.props.onClick(ind)}
+            />
+            )
+        }
+        
+    render() {
+        var tileSquares = [];
+        for (var i = 0; i < 25; i++){
+            tileSquares.push(this.renderTile(i));
+        }
+        return(
+            <section className="section">
+                <div className="board">
+                    {tileSquares}
+                </div>
+            </section>
+        )
+    }
+}
+        
+function Tile(props) {
     return(
-      <Square 
-        //assign random value (on /off)
-        //give onclick function
-      />
-    )
-  }
+        <button 
+        className={`square ${props.value === 0 ? 'unlit' : 'lit'}`} 
+        onClick={props.onClick}/>
+    );
+}
+function generateInitStates() {
+    var tiles = []
+    for (var i=0; i<25; i++) {
+        var randomBinary = Math.floor(Math.random()*2) // generates 0 or 1
+        tiles.push(randomBinary);
+    }
+    return tiles;
 }
 
-class Game extends Component {
-
+function toggleTiles(tiles, ind) {
+    const targetIndices = [ind, ind-5, ind-1, ind+1, ind+5];
+    const targetExistingIndices = targetIndices.filter(index => //filters out foul indices
+        (index >=0 && index<25) && 
+        (Math.abs(ind-index) > 1 || Math.floor(index/5) === Math.floor(ind/5))
+    );
+    for (var i = 0; i < targetExistingIndices.length; i++){
+        if (tiles[targetExistingIndices[i]] === 0) {
+            tiles[targetExistingIndices[i]] = 1;
+        } else {
+            tiles[targetExistingIndices[i]] = 0;
+        }
+    }
+    return tiles;
 }
+
+function didWinGame(tiles) {
+    for (var i = 0; i < tiles.length; i++){
+        if (tiles[i]===1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+export default Game;
